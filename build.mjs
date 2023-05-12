35d2c4a58060d8471e04e2389a38035491a33129
iiTONELOC <anthonytropeano@me.com>
2023-05-11 20:19:59 -0400

    Adjusted built scripts for Heroku

--- build.mjs
+++ build.mjs
@@ -1,26 +1,16 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { cwd } from 'process';
-import { exec } from 'child_process';
 
-exec('npm run lint && rimraf ./lib && tsc', (err, stdout, stderr) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(stdout);
-    console.log(stderr);
-}).on('close', () => {
-    const dirname = cwd();
+const dirname = cwd();
 
-    if (!fs.existsSync(path.join(dirname, './lib'))) {
-        fs.copyFileSync(
-            path.join(dirname, './src/graphql/schema.graphql'),
-            path.join(dirname, './lib/graphql/schema.graphql')
-        );
-    }
 
-    process.stdout.write('Build complete!\n');
-    process.exit(0);
-});
+if (fs.existsSync(path.join(dirname, 'lib', 'graphql'))) {
+    fs.copyFileSync(
+        path.join(dirname, 'src', 'graphql', 'schema.graphql'),
+        path.join(dirname, 'lib', 'graphql', 'schema.graphql')
+    );
+}
 
+process.stdout.write('Build complete!\n');
+process.exit(0);
