a1e38b23576edc735897741fd9e5642dd8628d78
iiTONELOC <anthonytropeano@me.com>
2024-02-05 16:30:56 -0500

    Create App User creation and Login db models, schema, controllers,
    and resolvers.

---
+++ src/db/Models/AppKey/index.ts
@@ -0,0 +1,65 @@
+import * as bcrypt from "bcrypt";
+import { Schema, model } from "mongoose";
+import { IDeviceKey, IDeviceKeyModel, IDeviceKeyMethods } from "../../types";
+
+// never rely on the hardcoded value!, it isn't secure and it is not even used in testing
+const defaultPepper = "aD*ex5898#!l;";
+// istanbul ignore next
+const pepper: string = process.env.PEPPER ?? defaultPepper;
+// istanbul ignore next
+const saltRounds: string = process.env.SALT_FACTOR ?? "10";
+
+const deviceKeySchema = new Schema<
+  IDeviceKey,
+  IDeviceKeyModel,
+  IDeviceKeyMethods
+>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    key: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      //must be a valid UUID4
+      // regex to match UUID v4 string
+      match: [
+        /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/,
+        "Please enter a valid device key!",
+      ],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // time in seconds until the API key expires
+      // keys should be valid for 30 days
+      expires: 2592000,
+    },
+  },
+  {
+    timestamps: false,
+    id: false,
+  }
+);
+
+// hash the key before saving it to the database
+// istanbul ignore next
+deviceKeySchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("key")) {
+    this.key = `${pepper}${this.key}`;
+    this.key = await bcrypt.hash(this.key, parseInt(saltRounds, 10));
+  }
+  next();
+});
+
+// compare the key to the hashed key in the database
+deviceKeySchema.method("isCorrectKey", async function (key: string) {
+  const isCorrect: boolean = await bcrypt.compare(`${pepper}${key}`, this.key);
+  return isCorrect;
+});
+
+export default model<IDeviceKey, IDeviceKeyModel>("AppKey", deviceKeySchema);
--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,4 +1,9 @@
-import { userController, listController, listItemController, userProductController } from '../controllers';
+import {
+  userController,
+  listController,
+  listItemController,
+  userProductController,
+} from "../controllers";
 
 const { userQueries, userMutations } = userController;
 const { listQueries, listMutations } = listController;
@@ -6,25 +11,28 @@ const { userProductMutations } = userProductController;
 const { listItemQueries, listItemMutations } = listItemController;
 
 export const resolvers = {
-    Query: {
-        me: userQueries.queryMe,
-        list: listQueries.list,
-        myLists: listQueries.myLists,
-        listItem: listItemQueries.listItem
-    },
-    Mutation: {
-        addUser: userMutations.addUser,
-        loginUserDevice: userMutations.loginUserDevice,
-        updateUser: userMutations.updateUser,
-        deleteUser: userMutations.deleteUser,
-        addList: listMutations.createList,
-        updateList: listMutations.updateList,
-        removeList: listMutations.deleteList,
-        addToList: listMutations.addToList,
-        removeFromList: listMutations.removeFromList,
-        updateListItem: listItemMutations.updateListItem,
-        generateDeviceKey: userMutations.generateDeviceKey,
-        addItemToDefaultList: listMutations.addItemToDefaultList,
-        updateUserProduct: userProductMutations.updateUserProduct,
-    }
+  Query: {
+    me: userQueries.queryMe,
+    list: listQueries.list,
+    myLists: listQueries.myLists,
+    listItem: listItemQueries.listItem,
+  },
+  Mutation: {
+    addUser: userMutations.addUser,
+    loginUserDevice: userMutations.loginUserDevice,
+    updateUser: userMutations.updateUser,
+    deleteUser: userMutations.deleteUser,
+    addList: listMutations.createList,
+    updateList: listMutations.updateList,
+    removeList: listMutations.deleteList,
+    addToList: listMutations.addToList,
+    loginUserApp: userMutations.loginUserApp,
+    addUserFromApp: userMutations.addUserFromApp,
+    removeFromList: listMutations.removeFromList,
+    updateListItem: listItemMutations.updateListItem,
+    generateAppKey: userMutations.generateAppKey,
+    generateDeviceKey: userMutations.generateDeviceKey,
+    addItemToDefaultList: listMutations.addItemToDefaultList,
+    updateUserProduct: userProductMutations.updateUserProduct,
+  },
 };
